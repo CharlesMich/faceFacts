621175b3bf87bed7d0568d16722cf4b34bde5bf2
CharlesMich <charleedongo@hotmail.com>
2025-04-15 17:43:10 -0500

    Initial Commit

---
+++ FaceFacts/FaceFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceFactsApp.swift
+//  FaceFacts
+//
+//  Created by Charles Michael on 4/15/25.
+//
+
+import SwiftUI
+
+@main
+struct FaceFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
