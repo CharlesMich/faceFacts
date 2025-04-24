70ca6fbf7af13193b91a28c6f6a72fb3aa7af43b
CharlesMich <charleedongo@hotmail.com>
2025-04-23 23:50:09 -0500

    apr 23

---
+++ FaceFacts/Event.swift
@@ -0,0 +1,21 @@
+//
+//  Event.swift
+//  FaceFacts
+//
+//  Created by Charles Michael on 4/23/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Event {
+    var name: String
+    var location: String
+    var people = [Person]()
+    
+    init(name: String, location: String) {
+        self.name = name
+        self.location = location
+    }
+}
